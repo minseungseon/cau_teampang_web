9051a98672a74537726673bd766fba9f582c0c00
MinseungSeon <seonminseung@naver.com>
2020-08-31 16:51:46 +0900

    [REFAC] : refactored team_time realted codes, working fine!

--- teampang/meeting/serializer.py
+++ teampang/meeting/serializer.py
@@ -14,21 +14,22 @@ class MeetingTimeSerializer(serializers.ModelSerializer):
 
 class MeetingCreateSerializer(serializers.ModelSerializer):
     team_input = MeetingInputSerializer(many=True)
-    # times = MeetingTimeSerializer(source='MeetingTime_set',many=True)
+    team_time = MeetingTimeSerializer(many=True)
 
     class Meta:
         model = MeetingCreate
             # fields = '__all__'
-        fields = ['name', 'due_date', 'invite_url', 'team_input']
+        fields = ['name', 'due_date', 'invite_url', 'team_input','team_time']
 
     def create(self, validated_data):
         inputs_data = validated_data.pop('team_input')
-        # times_data = validated_data.pop('times')
+        times_data = validated_data.pop('team_time')
         team = MeetingCreate.objects.create(**validated_data)
         for input_data in inputs_data:
-            # for time_data in times_data:
                 MeetingInput.objects.create(team=team, **input_data)
-                # MeetingInput.objects.create(team=team, **times_data)
+        for time_data in times_data:
+                MeetingInput.objects.create(team=team, **times_data)
+
         return team
 
 
