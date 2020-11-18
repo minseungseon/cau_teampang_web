d217aeb3324b3a760ee536e0b1e4ff20b5eca17e
LimGeon <lgun202@naver.com>
2020-11-19 04:10:17 +0900

    getPlanList modified but has Error

--- teampang/meeting/views.py
+++ teampang/meeting/views.py
@@ -21,7 +21,8 @@ class PlanViewSet(viewsets.ModelViewSet):
     @action(detail = True, methods = ["GET"])
     # 팀플 이름과 날짜만 포함된 리스트 데이터 가져오기
     def getPlanList(self, request, pk):
-        serializer = MainPagePlanListSerializer(queryset, many=True)
+        plan = self.get_object
+        serializer = MainPagePlanListSerializer(plan, many=True)
         return Response(serializer.data, status=200)
         
         
